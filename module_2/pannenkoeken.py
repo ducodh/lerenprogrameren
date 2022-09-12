8bd9f749acb8e870497d701c9d1be09bae4dc933
ducodh <99072498@mydavinci.nl>
2022-09-12 10:08:04 +0200

    mappen

---
+++ module_2/pannenkoeken.py
@@ -0,0 +1,15 @@
+pannenkoeken = 1
+eiren = 0.15
+bloem = 25
+melk = 40
+zout = 0.05
+boter = 2.5
+
+aantal = int(input("hoeveel pannenkoeken? "))
+
+print ("pannenkoeken " +str(pannenkoeken * aantal ))
+print ("eiren " + str(eiren * aantal))
+print ("bloem " + str(bloem * aantal))
+print ("melk " + str(melk * aantal))
+print ("zout " + str(zout * aantal))
+print ("boter " + str(boter * aantal))
