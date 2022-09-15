60783871dee12343470d47541fdd2f31094ecbda
ducodh <99072498@mydavinci.nl>
2022-09-15 09:27:46 +0200

    input en if-statement

---
+++ module_1/bon.py
@@ -0,0 +1,39 @@
+print("Albert hein")
+print(" ")
+aantal = 3
+omschrijving = " croky naturel 300g "
+stukprijs = 0.89
+totaalprijs = aantal * stukprijs
+
+print (str(aantal) + omschrijving + str(stukprijs)+ ' ' + str(totaalprijs))
+
+aantal2 = 12
+omschrijving2 = " redbull "
+stukprijs2 = 1.57
+totaalprijs2 = aantal2 * stukprijs2
+
+print (str(aantal2) + omschrijving2 + str(stukprijs2)+ ' ' + str(totaalprijs2))
+
+aantal3 = 2
+omschrijving3 = " friekandelbroodje "
+stukprijs3 = 1.23
+totaalprijs3 = aantal3 * stukprijs3
+
+print (str(aantal3) + omschrijving3 + str(stukprijs3)+ ' ' + str(totaalprijs3))
+
+aantal4 = 6
+omschrijving4 = " bannaan "
+stukprijs4 = 0.27
+totaalprijs4 = aantal4 * stukprijs4
+
+print (str(aantal4) + omschrijving4 + str(stukprijs4)+ ' ' + str(totaalprijs4))
+
+aantal5 = 1
+omschrijving5 = " blueband boter "
+stukprijs5 = 3.41
+totaalprijs5 = aantal5 * stukprijs5
+
+print (str(aantal5) + omschrijving5 + str(stukprijs5)+ ' ' + str(totaalprijs5))
+print (" ")
+print ("TOTAAL, ")
+print (totaalprijs + totaalprijs2 + totaalprijs3 + totaalprijs4 + totaalprijs5)
---
+++ module_2/min_max.py
@@ -0,0 +1,9 @@
+a = input("vul een geheel getal in: ")
+b = input("vul nog een geheel getal in: ")
+
+
+if a > b:
+    print ("a is het grootste getal")
+    max = a
+
+print (max)
